11f02ff95d37a93a852d5f7a1d704fea42476c51
Tom Brow <tebrow@gmail.com>
2013-02-11 14:05:14 -0600

    take source file as argument

--- main.hs
+++ main.hs
@@ -1,5 +1,29 @@
+import qualified System.Environment
+import System.Console.GetOpt as Options
 import Text.Nouns.Parser (parseFile)
 import Text.Nouns.Compiler (compile)
 import Text.Nouns.Renderer (render)
 
-main = parseFile "test.nouns" >>= putStr . render . compile
+main = do
+  (flags, files, errors) <- getOptions
+  runWithOptions flags files errors
+
+runWithOptions :: [Flag] -> [String] -> [String] -> IO ()
+runWithOptions [] [file] [] = do
+  parseFile file >>= putStr . render . compile
+runWithOptions _ _ _ = putStr helpText
+
+data Flag = Help
+
+optionsSpec :: [OptDescr Flag]
+optionsSpec =
+  [ Option ['h'] ["help"] (NoArg Help) "Print this help text."
+  ]
+
+getOptions :: IO ([Flag], [String], [String])
+getOptions = do
+  args <- System.Environment.getArgs
+  return (getOpt Permute optionsSpec args)
+
+helpText :: String
+helpText = usageInfo "Usage: nouns source.nouns" optionsSpec
