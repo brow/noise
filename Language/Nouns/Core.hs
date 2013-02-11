42a58962bae9624cc131d7741d6b48d338da3954
Tom Brow <tebrow@gmail.com>
2013-02-11 10:24:39 -0600

    print AST prettily

--- Language/Nouns/Core.hs
+++ Language/Nouns/Core.hs
@@ -3,7 +3,8 @@ module Language.Nouns.Core (document) where
 import Text.ParserCombinators.Parsec
 import qualified Language.Nouns.Token as Token
 
-data FunctionCall = FunctionCall FunctionName [Argument] deriving (Show)
+data Document = Document [FunctionCall] deriving Show
+data FunctionCall = FunctionCall FunctionName [Argument] deriving Show
 type FunctionName = String
 type Argument = Integer
 
@@ -13,8 +14,8 @@ functionCall = do
   args <- Token.parens (Token.commaSeparated Token.integer)
   return $ FunctionCall name args
 
-document :: Parser [FunctionCall]
+document :: Parser Document
 document = do
   Token.whiteSpace
   calls <- many functionCall
-  eof >> return calls
+  eof >> return (Document calls)
--- main.hs
+++ main.hs
@@ -1,5 +1,9 @@
 import Language.Nouns.Core
 import Text.Parsec.String
+import Text.Show.Pretty (ppShow)
 
 main = do
-  parseFromFile document "test.nouns" >>= print
+  result <- parseFromFile document "test.nouns"
+  case result of
+    Left err -> print err
+    Right ast -> putStr (ppShow ast)
