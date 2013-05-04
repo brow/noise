7319e12df08ed861be6ab51dfcfe1e555a73c2f2
Tom Brow <tebrow@gmail.com>
2013-05-03 23:44:25 -0500

    add `alpha` and `toRGBHex` functions

--- src/Text/Nouns/Compiler/Document/Color.hs
+++ src/Text/Nouns/Compiler/Document/Color.hs
@@ -1,7 +1,9 @@
 module Text.Nouns.Compiler.Document.Color
-( Color
+( Color(..)
 , toHex
 , fromHex
+, toRGBHex
+, alpha
 , black
 ) where
 
@@ -11,10 +13,7 @@ import Control.Applicative
 
 data Color = ARGB Word8 Word8 Word8 Word8
            | RGB Word8 Word8 Word8
-           deriving (Eq)
-
-instance Show Color where
-  show = ('#':) . toHex
+           deriving (Eq, Show)
 
 showHexByte :: Word8 -> String
 showHexByte = pad . flip showHex ""
@@ -32,6 +31,11 @@ toHex color = concatMap showHexByte $ case color of
   ARGB a r g b -> [a ,r, g, b]
   RGB r g b    -> [r, g, b]
 
+toRGBHex :: Color -> String
+toRGBHex color = toHex $ case color of
+  ARGB _ r g b -> RGB r g b
+  RGB {}       -> color
+
 fromHex :: String -> Maybe Color
 fromHex [a0,a1,r0,r1,g0,g1,b0,b1] = ARGB
   <$> readHexByte [a0,a1]
@@ -43,6 +47,10 @@ fromHex [r0,r1,g0,g1,b0,b1] = RGB
   <*> readHexByte [g0,g1]
   <*> readHexByte [b0,b1]
 fromHex _ = Nothing
+
+alpha :: Color -> Maybe Float
+alpha (ARGB a _ _ _) = Just (fromIntegral a / 255.0)
+alpha _              = Nothing
 
 black :: Color
 black = RGB 0 0 0
--- tests/Text/Nouns/Compiler/Document/Color/Test.hs
+++ tests/Text/Nouns/Compiler/Document/Color/Test.hs
@@ -5,6 +5,7 @@ module Text.Nouns.Compiler.Document.Color.Test where
 
 import Test.Framework
 import Control.Applicative
+import Data.Maybe (fromJust, isNothing, isJust)
 import qualified Text.Nouns.Compiler.Document.Color as Color
 
 {-# ANN module "HLint: ignore Use camelCase" #-}
@@ -20,3 +21,15 @@ instance Arbitrary HexColorString where
 prop_toHex_is_inverse_of_fromHex :: HexColorString -> Bool
 prop_toHex_is_inverse_of_fromHex (HexColorString hexStr) =
   Just hexStr == (Color.toHex <$> Color.fromHex hexStr)
+
+prop_toRGBHex_eq_toHex_over_RGB :: HexColorString -> Property
+prop_toRGBHex_eq_toHex_over_RGB (HexColorString str) =
+  (length str == 6) ==> Color.toRGBHex color == Color.toHex color
+  where color = fromJust (Color.fromHex str)
+
+prop_alpha_is_Nothing_over_RGB :: HexColorString -> Bool
+prop_alpha_is_Nothing_over_RGB (HexColorString str)
+  | length str == 6 = isNothing alpha
+  | length str == 8 = isJust alpha
+  | otherwise       = False
+  where alpha = Color.fromHex str >>= Color.alpha
